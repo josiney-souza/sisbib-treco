873e8da04526671fa6730859cea8609a240d05fa
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-04-13 17:13:39 -0300

    Adicionada consulta elaborada "livros compartilhados por mais de um componente"
    Adicionada a consulta elaborada "Relação de todos os livros compartilhados por mais de um componente curricular" no arquivo consultas.php

--- consultas.php
+++ consultas.php
@@ -215,6 +215,40 @@ try {
   	}
   	echo "\t</table>\n";
   }
+  elseif ($table == "con12") {
+  	echo "\t<h2>Relação de todos os livros compartilhados por mais de uma disciplina</h2>\n";
+  	echo "\t<table>\n";
+  	echo "\t\t<tr>\n";
+  	echo "\t\t\t<th>#</th>\n";
+  	echo "\t\t\t<th>Autor</th>\n";
+  	echo "\t\t\t<th>Título</th>\n";
+  	echo "\t\t\t<th>Componente Curricular</th>\n";
+  	echo "\t\t\t<th>Semestre</th>\n";
+  	echo "\t\t</tr>\n";
+  	foreach($db->query("SELECT DISTINCT
+	  		l1.autor as Autor,
+			l1.titulo as Título,
+			cc1.cc as Componente_Curricular,
+			cc1.semestre as Semestre
+		FROM
+			Usos as u1, Livros as l1, Tipos as t1, Componentes_Curriculares as cc1,
+			Usos as u2, Livros as l2, Tipos as t2, Componentes_Curriculares as cc2
+		WHERE
+			(u1.livro = l1.id AND u1.tipo = t1.id AND u1.cc = cc1.id)
+			AND (u2.livro = l2.id AND u2.tipo = t2.id AND u2.cc = cc2.id)
+			AND (l1.autor = l2.autor AND l1.titulo = l2.titulo AND l1.subtitulo = l2.subtitulo AND cc1.cc <> cc2.cc)
+		ORDER BY l1.titulo, l1.autor, cc1.semestre") as $row) {
+    	echo "\t\t<tr>\n";
+  		echo "\t\t\t<td>$num_linha</td>\n";
+    	echo "\t\t\t<td>" . $row['Autor'] . "</td>\n";
+    	echo "\t\t\t<td>" . $row['Título'] . "</td>\n";
+    	echo "\t\t\t<td>" . $row['Componente_Curricular'] . "</td>\n";
+    	echo "\t\t\t<td>" . $row['Semestre'] . "</td>\n";
+    	echo "\t\t</tr>\n";
+    	$num_linha++;
+  	}
+  	echo "\t</table>\n";
+  }
   else {
   	echo "\t<h2>Função ainda não implementada</h2>\n";
   }
